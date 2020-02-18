742d6f8bb9aee8a6a63fe92af1216f58840e9e7b
taro.kirameki <ptest2020@jrptelevision.com>
2020-02-18 08:40:59 +0000

    last

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,64 @@
+class TasksController < ApplicationController
+  def index
+      @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+     @task = Task.new(task_params)
+     
+     if @task.save
+        flash[:success] = 'Message が正常に投稿されました。'
+        redirect_to @message_params
+        
+     else
+         flash.now[:danger] = 'Messageが投稿されませんでした。'
+         render :new
+         
+     end
+  end
+
+  def edit
+      @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to tasks_url
+  end
+end
+
+
+  private
+  
+  
+  
+  def task_params
+     params.require(:task).permit(:content)
+  end
+  
+  
+
+
