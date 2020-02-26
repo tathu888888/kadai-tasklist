fa572e1223458fbd4ec65ede348dc5941368bc84
taro.kirameki <ptest2020@jrptelevision.com>
2020-02-26 08:15:32 +0000

    test1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
     @user = User.new
 
-  end
+    end
 
 
 
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
