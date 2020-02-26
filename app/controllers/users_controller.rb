40086ba043f2ff4b1bca8e88d46c4c30973a77ce
taro.kirameki <ptest2020@jrptelevision.com>
2020-02-26 08:10:35 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,13 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
 
-  
+    def new
+
+    @user = User.new
+
+  end
+
+
 
   def create
     @user = User.new(user_params)
